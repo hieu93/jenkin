16ad7a3bf3895323f750ee9e8bfd2ab7cca46096
hieu93 <trieutrunghieu176@gmail.com>
2023-08-20 09:06:58 +0700

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-echo "Hello"
-git add .
-git commit -m "update"
-git config --global user.email "trieutrunghieu176@gmail.com"
-git config --global user.name "hieu93"
-git push origin HEAD:main
+echo "Test demojob1"
+
